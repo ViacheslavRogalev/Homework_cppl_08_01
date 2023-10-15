03e95f1b146fc403da2298f68c6e30a871afe927
vyahislav <khv-rvv@mail.ru>
2023-10-15 20:11:06 +1000

    first commit

---
+++ main.cpp
@@ -0,0 +1,34 @@
+﻿#include<iostream>
+#include<vector>
+#include<algorithm>
+
+void unique(std::vector<int>& v)
+{
+	std::sort(v.begin(), v.end());
+	auto result = std::unique(begin(v), end(v));
+	v.erase(result, v.end());
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+	std::vector<int> vec{1, 1, 2, 5, 6, 1, 2, 4};
+
+	std::cout << "\n[IN]: ";
+	for (auto& it : vec)
+	{
+		std::cout << it << " ";
+	}
+
+	unique(vec);
+	std::cout << "\n[OUT]: ";
+	for (auto& it : vec)
+	{
+		std::cout << it << " ";
+	}
+
+	std::cout << "\n";
+
+	return 0;
+}
